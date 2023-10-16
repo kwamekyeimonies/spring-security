aece3ba74a56edf2863e0d46c3dc7f743d173a6b
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-10-16 20:06:50 +0000

    springsecurity integration with keycloak

---
+++ src/test/java/com/teacodesecurity/teacodesecurity/TeacodesecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teacodesecurity.teacodesecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacodesecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
