52ef051a65916c3df66270f52905dc3e7bc368e7
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-10-17 09:13:11 +0000

    staging

--- src/main/java/com/teacodesecurity/teacodesecurity/controller/DemoController.java
+++ src/main/java/com/teacodesecurity/teacodesecurity/controller/DemoController.java
@@ -1,10 +1,17 @@
 package com.teacodesecurity.teacodesecurity.controller;
 
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/demo")
 public class DemoController {
+
+    @GetMapping
+    public ResponseEntity<String> greetings(){
+        return ResponseEntity.ok("Welcome to tea code development");
+    }
 }
--- src/main/java/com/teacodesecurity/teacodesecurity/controller/config/SecurityConfig.java
+++ src/main/java/com/teacodesecurity/teacodesecurity/controller/config/SecurityConfig.java
@@ -5,28 +5,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    @Bean
-    public SecurityWebFilterChain securityWebFilterChain(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity
-                .csrf()
-                .disable()
-                .authorizeHttpRequests()
-                .anyRequest()
-                .authenticated();
-
-        httpSecurity
-                .oauth2ResourceServer()
-                .jwt();
-
-        httpSecurity
-                .sessionManagement()
-                .sessionCreationPolicy(STATELESS);
-    }
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity
+//                .csrf()
+//                .disable()
+//                .authorizeHttpRequests()
+//                .anyRequest()
+//                .authenticated();
+//        httpSecurity
+//                .oauth2ResourceServer()
+//                .jwt();
+//        httpSecurity
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//
+//        return httpSecurity.build();
+//    }
 }
