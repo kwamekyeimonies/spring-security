5387d32988fe4c75c8c75e7e6e8bde5159d40d9f
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-10-22 23:08:46 +0000

    project restructuring && deprecated modules fixed

---
+++ keycloak-authentication/src/test/java/com/keycloackauthentication/keycloackauthentication/KeycloackAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keycloackauthentication.keycloackauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloackAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
