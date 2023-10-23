54f491d2b929ab2dc9522c6663cc8a1f1f7f5546
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-10-23 20:22:54 +0000

    openid-connect implementation

---
+++ openId-connect-authentication/src/main/java/com/teacodesecurity/openIdconnectauthentication/OpenIdConnectAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.teacodesecurity.openIdconnectauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenIdConnectAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenIdConnectAuthenticationApplication.class, args);
+	}
+
+}
---
+++ openId-connect-authentication/src/test/java/com/teacodesecurity/openIdconnectauthentication/OpenIdConnectAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teacodesecurity.openIdconnectauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenIdConnectAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
