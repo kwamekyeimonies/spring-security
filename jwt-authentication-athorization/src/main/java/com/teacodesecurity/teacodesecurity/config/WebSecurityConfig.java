768705249f360d1245caf2233c691a53d069c6d6
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-12-03 03:47:54 +0000

    2MFA multifactor authentication with google authenticator

--- jwt-authentication-athorization/src/main/java/com/teacodesecurity/teacodesecurity/config/WebSecurityConfig.java
+++ jwt-authentication-athorization/src/main/java/com/teacodesecurity/teacodesecurity/config/WebSecurityConfig.java
@@ -6,7 +6,6 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
-import com.nimbusds.jwt.JWT;
 import com.teacodesecurity.teacodesecurity.utility.RSAKeyProperties;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
